6e1d360292cef5ab2e2536881ae992a62b44b7c6
Famanias <haratayo@gmail.com>
2025-03-21 21:14:40 +0800

    dereployment

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: ''  // Update after backend deployment
+    apiUrl: 'https://darwin-kpi-backend.vercel.app'  // Update after backend deployment
   };
